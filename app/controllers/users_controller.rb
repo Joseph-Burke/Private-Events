db1b25607200e4cc51e202eec17e8336692b391f
Joseph Burke <joseph_burke@outlook.com>
2020-09-15 11:14:22 +0100

    Work on controller and views for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,26 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+# No method error for #create. Therefore, @user is currently nil.
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_url
+      flash[:success] = 'User creation unsuccessful.'
+    else
+      flash.now[:alert] = 'User creation unsuccessful.'
+    end
   end
 
   def show
-    redirect_to users_path
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 
 end
