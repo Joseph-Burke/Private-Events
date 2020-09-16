bb9d91678e01d294b20f58a2ea129ffd999c0f8c
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-09-16 10:39:56 +0100

    Add Sessions Contoller, Its methods and Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[new create show]
+  # 'post''session#create'
+  # 'get''session#new'
+  # 'get''session#destroy'
+  get 'sessions/new', to: 'sessions#new' 
+  post 'sessions', to: 'sessions#create'
+  delete 'sessions', to: 'sessions#destroy'
+  
 end
