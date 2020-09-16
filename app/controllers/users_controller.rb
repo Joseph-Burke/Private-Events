5cc564de4cee52b72e10229f3b4f86b14135201c
Joseph Burke <joseph_burke@outlook.com>
2020-09-16 08:47:21 +0100

    Fix slight bugs in user controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path(@user)
+      redirect_to user_path(@user)
       flash[:success] = 'User creation unsuccessful.'
     else
       flash.now[:alert] = 'User creation unsuccessful.'
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:format])
+    @user = User.find(params[:id])
   end
 
   private
@@ -24,5 +24,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name)
   end
-
 end
