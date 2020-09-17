317b1627cab707433ee541c82a182b7d855d8e70
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-09-17 13:28:59 +0100

    Remove commented code in event.rb and eventattendee.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,20 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-
-  # Link to the through table. E.g, has many event_attendees
   has_many :event_attendees, foreign_key: :attended_event_id
-  # Complete the many-to-many relationship by linking through the "through table".
   has_many :attendees, through: :event_attendees
   
    
-  # def Event.past
-  #   Event.all.select { |event| event.date < Date.today }
-  # end
-  
-  # def Event.upcoming
-    
-  # end
-  
   scope :past, -> {Event.all.select { |event| event.date < Date.today }}
   scope :upcoming , -> {Event.all.select { |event| event.date >= Date.today }}
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: :creator_id
-
-  # Link to the through table. E.g, has many event_attendees
-  
   has_many :event_attendees, foreign_key: :attendee_id
   has_many :attended_events, through: :event_attendees
 
