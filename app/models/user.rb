d056fef5827f828fed393d17e55fb09cd43cef2b
Joseph Burke <joseph_burke@outlook.com>
2020-09-17 10:56:15 +0100

    Modify events/show.html.erb to display list of attendees

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :events, foreign_key: :creator_id
 
   # Link to the through table. E.g, has many event_attendees
+  
   has_many :event_attendees, foreign_key: :attendee_id
-
   has_many :attended_events, through: :event_attendees
 
 
